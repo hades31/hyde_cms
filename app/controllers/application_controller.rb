1e76ddb44d4d85ad671d22e9e08afce291340716
Hades <hades3191@gmail.com>
2017-06-17 06:47:25 +0800

    Updated front-end block section incomplete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_filter :build_page_nav
+  before_filter :build_page_nav, :build_jumbotron_blocks, :build_blocks
   protect_from_forgery with: :exception
 
   def build_page_nav
     @page_nav = Page.where(menu_display: true, is_published: true).order(order: :asc)
+  end
+
+  def build_jumbotron_blocks
+    @jumbotron = Block.where(position: 'block')
+  end
+
+  def build_blocks
+    @blocks = Block.where(position: 'block').order(order: :asc)
   end
 end
